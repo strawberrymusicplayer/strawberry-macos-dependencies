ae44a309d3e90836b2467b91d016931290523832
Jonas Kvinge <jonas@jkvinge.net>
2026-02-09 23:42:46 +0100

    Remove rapidjson

--- scripts/update.sh
+++ scripts/update.sh
@@ -374,9 +374,6 @@ function update_package() {
     "sparsehash")
       package_version_latest=$(latest_github_release "sparsehash" "sparsehash")
       ;;
-    "rapidjson")
-      package_version_latest=$(latest_github_release "Tencent" "rapidjson")
-      ;;
     *)
       package_version_latest=
       error "No update rule for package: ${package}"
